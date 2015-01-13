5a01221fe5fff80449d765f8e66a0d33665e75d5
Krailon <root@rpi.(none)>
2015-01-12 18:19:53 -0800

    Added * expansion; Added byte field format support; Updated format definition file parsing to JSON

--- prototype/id.py
+++ prototype/id.py
@@ -4,10 +4,10 @@
 # By Lee & Matt
 #
 
-import sys, os, argparse, re
+import sys, os, argparse, re, json
 
 parser = argparse.ArgumentParser(description="Prototype Biological File Format Identifier")
-parser.add_argument("-f", action="store", dest="FormatsPath", help="The path to the format definition file, defaults to formats.dat")
+parser.add_argument("-f", action="store", dest="FormatsPath", help="The path to the format definition file, defaults to formats.json")
 parser.add_argument("inputs", metavar="InputFiles", nargs="+", help="The input files whose formats are to be identified")
 args = parser.parse_args()
 
@@ -15,32 +15,48 @@ if len(sys.argv) < 2:
 	exit(1)
 
 if args.FormatsPath == None:
-	# Default format definition file: formats.dat
-	args.FormatsPath = "formats.dat"
+	# Default format definition file: formats.json
+	args.FormatsPath = "formats.json"
 
 if not os.path.exists(args.FormatsPath):
 	print "Error: Failed to find " + args.FormatsPath
 	exit(1)
 
-# Load definitions
-fdf = open(args.FormatsPath, "r")
-lines = fdf.read().splitlines()
-fdf.close()
+# <Shiny> Replace wildcard ("*") with list of local files
+if "*" in args.inputs:
+	args.inputs.remove("*")
+	for (dirPaths, dirNames, fileNames) in os.walk("."):
+		for fileName in fileNames:
+			if not fileName in args.inputs:
+				args.inputs.append(fileName)
 
-# Parse definitions
-formats = []
-for line in lines:
-	formats.append(line.split("\t", 1)) # Format name and regex are tab-separated
+# Load & parse definitions
+with open(args.FormatsPath, "r") as hDef:
+	conts = hDef.read()
+formats = json.loads(conts)["formats"]
 
 # Process input files
 recog = []
 for filePath in args.inputs:
 	with open(filePath, "r") as inFile:
 		buff = inFile.read() # << Is it worth checking filesize and breaking up the reads if over a threshold? >>
-		for format in formats:
-			if re.findall(format[1].replace("\\n", "\n"), buff) != []:
-				print filePath + ": " + format[0]
+
+	for format in formats:
+		if format["type"] == "regex":
+			if re.findall(format["regex"].replace("\\n", "\n"), buff) != []:
+				print filePath + ": " + format["name"]
 				recog.append(filePath)
+				break
+		elif format["type"] == "bytes":
+			matched = True
+			for bytes in format["bytes"]:
+				if not bytes in buff:
+					matched = False
+					break
+
+			# Move on if any byte field doesn't match
+			# << Idea: Add "requirement" attribute ("required" or "optional") to byte fields >>
+			if not matched:
 				break
 
 	# Notify if unrecognized
