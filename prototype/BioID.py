5ca2b37e039356e60b34297252b491c38223b5be
Lee Bergstrand <leemacboy@gmail.com>
2015-01-21 14:33:21 -0800

    Class method was actually not nessary. @classmethod decorator is only for class methods (which is different then the methods of a class). Variables our side of functions in a class definition are global scope amount all instances of the same class and therefore were removed.

--- prototype/BioID.py
+++ prototype/BioID.py
@@ -9,30 +9,28 @@ import mmap
 
 
 class BioID:
-	definitions = None
-
 	def __init__(self, path):
 		with open(path, "rU") as definitionfile:
 			contents = definitionfile.read()
 		self.definitions = json.loads(contents)["formats"]
 
-	@classmethod
-	def identify(cls, files):
+	def identify(self, files):
 		identified = {}
+		matched = False
 		for filepath in files:
 			with open(filepath, "rU") as inputfile:
-				filecontents = inputfile.read()
+				infile = inputfile.read()
 				mappedfile = mmap.mmap(inputfile.fileno(), 0, mmap.MAP_PRIVATE, mmap.PROT_READ)
 
-			if len(filecontents) == 0:
+			if len(infile) == 0:
 				identified[filepath] = "empty"  # Empty files have no format :)
 				continue
 
-			for definition in cls.definitions:
+			for definition in self.definitions:
 				matched = True
 				if "regexen" in definition:
 					for regex in definition["regexen"]:
-						if not re.findall(regex.replace("\\n", "\n"), filecontents, re.IGNORECASE):
+						if not re.findall(regex.replace("\\n", "\n"), infile, re.IGNORECASE):
 							matched = False
 							break
 			if "bytes" in definition:
