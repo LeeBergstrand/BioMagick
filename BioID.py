b1c6006a323981d260712f4836a56fc75cc41f26
Lee Bergstrand <leemacboy@gmail.com>
2015-02-15 13:24:35 -0800

    Cleaned up code in BioID and text_BioID:
    - Added cleaned up and more readable comments.
    - Removed double returning from BioID.identify_text_file and BioID.identify_binary_file
    - Pulled text identification code out into its own method for use later (e.g.. identify from stdin.)

--- BioID.py
+++ BioID.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-#
-# A class for auto identifying BioInformatics file formats.
-# By Lee & Matt
+
+# ============================================================================================================
+# Created by: Lee Bergstrand & Matt McInnes
+# Description: A class for identifying bioinformatic file formats.
+# Requirements: This class requires the binaryornot module: https://pypi.python.org/pypi/binaryornot/
+# ============================================================================================================
 
 import sys
 import re
@@ -11,6 +14,7 @@ import codecs
 from binaryornot.check import is_binary
 
 
+# ============================================================================================================
 # BioFormat: Defines the properties of a bioinformatic file format object:
 class BioFormat(object):
 	def __init__(self, name, file_type, compression, markers):
@@ -20,6 +24,7 @@ class BioFormat(object):
 		self.markers = list(markers)
 
 
+# ============================================================================================================
 # BioFormat: Defines the properties of a bioinformatic file identification object:
 class BioID(object):
 	def __init__(self, path):
@@ -38,9 +43,27 @@ class BioID(object):
 				self.text_definitions.append(
 					BioFormat(definition["name"], definition["type"], definition["compression"], definition["markers"]))
 
-	# Method used to search binary files for raw byte sequences
-	def identify_binary(self, file_path):
+	# ----------------------------------------------------------------------------------------------
+	# Method: Used to identify a list of bioinformatic files.
+	# ----------------------------------------------------------------------------------------------
+	def identify(self, files):
+		identified = {}
+
+		# Check if each each file is binary or text and use the appropriate marker (regex or byte field).
+		for file_path in files:
+			if is_binary(file_path):
+				identified[file_path] = self.identify_binary_file(file_path)
+			else:
+				identified[file_path] = self.identify_text_file(file_path)
+
+		return identified
+
+	# ---------------------------------------------------------------------------------------------------
+	# Method: Used to determine if input binary file is a bioinformatic file by using raw byte sequences.
+	# ---------------------------------------------------------------------------------------------------
+	def identify_binary_file(self, file_path):
 		binary_input_file = open(file_path, "rb")  # Read in binary file as binary ("rb")
+		identity = "unrecognized"
 
 		if sys.platform == "win32":  # Check if os is windows
 			mapped_file = mmap.mmap(binary_input_file.fileno(), 0, None, mmap.ACCESS_READ)
@@ -57,19 +80,27 @@ class BioID(object):
 					pattern_match = False
 					break  # All byte fields must be present in order for the file type check to pass
 			if pattern_match:
-				mapped_file.close()
-				binary_input_file.close()
-				return binary_file_type.name
+				identity = binary_file_type.name
+				break
 
 		mapped_file.close()
 		binary_input_file.close()
-		return "unrecognized"
+		return identity
 
-	# Method used to match regular expressions against "text" files
-	def identify_text(self, file_path):
-		text_input_file = open(file_path, "rU")  # Read in text file as text with universal newlines ("rU")
-		input_text = text_input_file.read()
+	# ----------------------------------------------------------------------------------------------
+	# Method: Used to determine if input text file is a bioinformatic file by using REGEXs.
+	# ----------------------------------------------------------------------------------------------
+	def identify_text_file(self, file_path):
+		with open(file_path, "rU") as text_input_file:  # Read in text file as text with universal newlines ("rU")
+			input_text = text_input_file.read()
+			identity = self.identify_raw_text(input_text)
+			return identity
 
+	# ----------------------------------------------------------------------------------------------
+	# Method: Used to determine if raw text is a bioinformatic file by using REGEXs.
+	# ----------------------------------------------------------------------------------------------
+	def identify_raw_text(self, input_text):
+		output = "unrecognized"
 		for text_file_type in self.text_definitions:
 			pattern_match = False
 			for regex in text_file_type.markers:
@@ -80,21 +111,8 @@ class BioID(object):
 					pattern_match = False
 					break
 			if pattern_match:
-				text_input_file.close()
-				return text_file_type.name
+				output = text_file_type.name
+				break
 
-		text_input_file.close()
-		return "unrecognized"
+		return output
 
-	# Method one used for identifying the file type of a list for files:
-	def identify(self, files):
-		identified = {}
-
-		# Check if each each file is binary or text and use the appropriate marker (regex or byte field).
-		for file_path in files:
-			if is_binary(file_path):
-				identified[file_path] = self.identify_binary(file_path)
-			else:
-				identified[file_path] = self.identify_text(file_path)
-
-		return identified
