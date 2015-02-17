aee0e79ff3726b07285febb19df7f5873097a5b6
Lee Bergstrand <leemacboy@gmail.com>
2015-02-16 23:17:49 -0800

    Started conversion class.

--- BioMagick.py
+++ BioMagick.py
@@ -10,7 +10,10 @@ from Bio import SeqIO
 from Bio import AlignIO
 from Bio import Phylo
 from BioID import BioID
+import ntpath
+import os
 import argparse
+import sys
 
 # -------------------------------
 # Command line interface options.
@@ -32,7 +35,53 @@ args = parser.parse_args()
 # Command line interface Controller.
 # ----------------------------------
 def cli():
-	print(args.input)
+	input_files = args.input
+	out_fmt = args.outfmt
+	if args.outdir:
+		os.chdir(args.outdir)  # Set working directory of script to output dir.
+
+	id_results = BioID("./formats.yml").identify(input_files)
+
+	if out_fmt:
+		direct_convert(id_results, out_fmt)
+
+# ------------------------------------------------------------
+# Generates of dictionary of sequence record objects per file.
+# ------------------------------------------------------------
+def generate_sequence_objects(id_results):
+	seq_objects = {}
+
+	for file_path, identity in id_results.items():
+		with open(file_path, 'rU') as input_handle:
+			new_seq_objects = SeqIO.parse(input_handle, identity)
+			seq_objects[file_path] = new_seq_objects
+
+	return seq_objects
+
+
+# ------------------------------------------------------------
+# Generates of dictionary of sequence record objects per file.
+# ------------------------------------------------------------
+def direct_convert(id_results, out_fmt):
+
+	for format_type in out_fmt:
+		for file_path, identity in id_results.items():
+
+			if sys.platform == "win32":
+				file_name = ntpath.basename(file_path).split('.')[0]
+			else:
+				file_name = os.path.basename(file_path).split('.')[0]
+
+				file_name = file_name + "." + format_type
+
+			print("Converting " + file_path + " to " + format_type)
+
+			try:
+				SeqIO.convert(file_path, identity.lower(), file_name, format_type)
+			except ValueError as e:
+				print("\nError in conversion of " + file_path + " to " + format_type + ": " + str(e))
+				print("Skipping " + file_path + " ...\n")
+				continue
 
 if __name__ == '__main__':
 	cli()
