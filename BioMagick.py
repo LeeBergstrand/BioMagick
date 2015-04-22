8eee78b28380b99c513ae979fab03ffcdd893cc9
Krailon <mcinnes.mj@gmail.com>
2015-04-22 14:23:58 -0700

    Added parallel processing for multiple files; Added CLI flag to print single-file conversion result to stdout

--- BioMagick.py
+++ BioMagick.py
@@ -18,6 +18,8 @@ import os
 import argparse
 import sys
 import yaml
+import multiprocessing
+import functools
 
 
 class BioMagickFormat(BioIDFormat):
@@ -38,6 +40,13 @@ def main(args):
 	if out_fmt is None or out_fmt == []:
 		print("Error: at laest 1 output format is needed")
 		exit(1)
+
+	if args.stdout is True:
+		if len(input_files) > 1:
+			print("Error: outputting to stdout is only possible with single-file conversions")
+			exit(1)
+		else:
+			out_dir = None  # Indicate to use stdout
 
 	if args.alphabet is not None:
 		alphabet = args.alphabet[0]
@@ -60,7 +69,7 @@ def main(args):
 			print("Valid alphabets: ambigdna, unambigdna, exdna, ambigrna, unambigrna, prot, exprot")
 			exit(1)
 	else:
-		alphabet = ""
+		alphabet = None
 
 	# Load and parse YAML format export settings
 	with open("BioMagickFormatInfo.yml", "rU") as settings_file:
@@ -71,14 +80,20 @@ def main(args):
 		settings[setting["name"]] = BioMagickFormat(setting["name"], setting["extension"], setting["bioclass"])
 
 	if input_files is None or input_files == []:
-		id_results = BioID("./BioIDFormatInfo.yml").identify(input_files)
-	else:
+		# Convert single file from stdin
 		if sys.stdin.isatty():
 			print("Error: you must either specify an input file or pipe some data to stdin")
 			exit(1)
-		id_results = BioID("./BioIDFormatInfo.yml").identify(sys.stdin.read())
 
-	direct_convert(settings, id_results, out_dir, out_fmt, alphabet)
+		id_results = BioID("./BioIDFormatInfo.yml").identify(sys.stdin.read())
+		direct_convert(settings, id_results, out_dir, out_fmt, alphabet)
+	elif len(input_files) == 1:
+		id_results = BioID("./BioIDFormatInfo.yml").identify(input_files)
+		direct_convert(settings, id_results, out_dir, out_fmt, alphabet)
+	else:
+		pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+		pool.map(functools.partial(do_conversion, format_settings=settings, output_path=out_dir, output_formats=out_fmt,
+		                           input_alphabet=alphabet), input_files)
 
 
 # ------------------------------------------------------------
@@ -95,43 +110,74 @@ def generate_sequence_objects(id_results):
 	return seq_objects
 
 
+def do_conversion(input_file, format_settings, output_path, output_formats, input_alphabet):
+	id_results = BioID("./BioIDFormatInfo.yml").identify([input_file])
+	direct_convert(format_settings, id_results, output_path, output_formats, input_alphabet)
+
+
 # ------------------------------------------------------------
 # Generates of dictionary of sequence record objects per file.
 # ------------------------------------------------------------
 def direct_convert(settings, id_results, out_path, out_formats, alphabet):
-	for out_format in out_formats:
-		for in_path, in_format in id_results.items():
-			out_file = out_path
-			if sys.platform == "win32":
-				if out_file[-1] != "/":
-					out_file += "/"
+	if out_path is None:
+		out_file = "./conv.tmp"
+		in_path, in_format = list(id_results.items())[0]
+		out_format = out_formats[0]
 
-				out_file += ntpath.basename(in_path).split('.')[0]
+		try:
+			format_setting = settings[in_format]
+			if format_setting.bioclass == "seq":
+				SeqIO.convert(in_path, in_format.lower(), out_file, out_format, alphabet)
+			elif format_setting.bioclass == "phylo":
+				Phylo.convert(in_path, in_format.lower(), out_file, out_format)
+			elif format_setting.bioclass == "align":
+				AlignIO.convert(in_path, in_format.lower(), out_file, out_format)
 			else:
-				if out_file[-1] != "\\":
-					out_file += "\\"
+				print("Error: invalid BioPython conversion class: %s" % format_setting.bioclass)
+				exit(1)
+		except ValueError as e:
+			print("Error in conversion of " + in_path + " to " + out_format + ": " + str(e))
+			exit(1)
 
-				out_file += os.path.basename(in_path).split('.')[0]
+		with open(out_file, "r") as tmp_file:
+			print(tmp_file.read())
 
-			out_extension = settings[out_format].extension
-			out_file = out_file + "." + out_extension
-			print("Converting %s file %s to %s file %s" % (in_format, in_path, out_format, out_file))
+		os.remove(out_file)  # Is this really necessary?
+	else:
+		for out_format in out_formats:
+			for in_path, in_format in id_results.items():
+				out_file = out_path
+				if sys.platform == "win32":
+					if out_file[-1] != "/":
+						out_file += "/"
 
-			try:
-				format_setting = settings[id_results[in_path]]
-				if format_setting.bioclass == "seq":
-					SeqIO.convert(in_path, in_format.lower(), out_file, out_format, alphabet)
-				elif format_setting.bioclass == "phylo":
-					Phylo.convert(in_path, in_format.lower(), out_file, out_format)
-				elif format_setting.bioclass == "align":
-					AlignIO.convert(in_path, in_format.lower(), out_file, out_format)
+					out_file += ntpath.basename(in_path).split('.')[0]
 				else:
-					print("Error: invalid BioPython conversion class: %s" % format_setting.bioclass)
-					exit(1)
-			except ValueError as e:
-				print("\nError in conversion of " + in_path + " to " + out_format + ": " + str(e))
-				print("Skipping " + in_path + " ...\n")
-				continue
+					if out_file[-1] != "\\":
+						out_file += "\\"
+
+					out_file += os.path.basename(in_path).split('.')[0]
+
+				out_extension = settings[out_format].extension
+				out_file = out_file + "." + out_extension
+				print("Converting %s file %s to %s file %s" % (in_format, in_path, out_format, out_file))
+
+				try:
+					format_setting = settings[in_format]
+					if format_setting.bioclass == "seq":
+						SeqIO.convert(in_path, in_format.lower(), out_file, out_format, alphabet)
+					elif format_setting.bioclass == "phylo":
+						Phylo.convert(in_path, in_format.lower(), out_file, out_format)
+					elif format_setting.bioclass == "align":
+						AlignIO.convert(in_path, in_format.lower(), out_file, out_format)
+					else:
+						print("Error: invalid BioPython conversion class: %s" % format_setting.bioclass)
+						exit(1)
+				except ValueError as e:
+					print("\nError in conversion of " + in_path + " to " + out_format + ": " + str(e))
+					print("Skipping " + in_path + " ...\n")
+					continue
+
 
 if __name__ == '__main__':
 	# -------------------------------
@@ -139,7 +185,10 @@ if __name__ == '__main__':
 	# -------------------------------
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-i', '--input', metavar='INPATH', nargs='+', help='''
-	A list of input file paths. If not specified, input is read from stdin''')
+	A list of input file paths. If not specified, input is read from stdin.''')
+
+	parser.add_argument('-s', '--stdout', dest="stdout", action='store_true', help='''
+	Output result of single-file conversion to stdout.''')
 
 	parser.add_argument('-o', '--outdir', metavar='OUTPATH', nargs=1, help='''
 	An output directory for output files. If not specified, output is piped to stdout.''')
